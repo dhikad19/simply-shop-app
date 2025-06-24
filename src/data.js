f7547793d291baef837c7ac2cfc914cccdaa8980
dikad911 <dwi.andika@kbru.co.id>
2025-06-24 11:54:17 +0700

    update images

--- src/data.js
+++ src/data.js
@@ -1,91 +1,89 @@
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/965324/pexels-photo-965324.jpeg?_gl=1*1d7k5ru*_ga*MjUxMzg2MTIuMTc1MDc0MDY3NQ..*_ga_8JE65Q40S6*czE3NTA3NDA2NzUkbzEkZzEkdDE3NTA3NDA3NzYkajYwJGwwJGgw",
+    title: "Women Fashion",
+    desc: "Lot of fashion available here. New design will be update for every week, check out our new dress.",
+    cat: "women",
+    to: "productlist",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1620760/pexels-photo-1620760.jpeg?_gl=1*1twscqc*_ga*MjUxMzg2MTIuMTc1MDc0MDY3NQ..*_ga_8JE65Q40S6*czE3NTA3NDA2NzUkbzEkZzAkdDE3NTA3NDA2NzUkajYwJGwwJGgw",
+    title: "Kids Fashion",
+    desc: "We have many fashion and color for your kids. New design will arrive soon, and see our latest t-shirt.",
+    cat: "kids",
+    to: "productlist",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?_gl=1*2xb30r*_ga*MTA2ODE2ODEyNC4xNzUwNzQwNDc4*_ga_8JE65Q40S6*czE3NTA3NDA0NzckbzEkZzEkdDE3NTA3NDA2MzkkajgkbDAkaDA.",
+    title: "Man Fashion",
+    desc: "Cool and many fashion for you. Design by popular fashion stylish, we have new model of jacket, grab now.",
+    cat: "men",
+    to: "productlist",
+  },
+];
 
-  
-  export const categories = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/10012719/pexels-photo-10012719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650w=940",
-      title: "Women Fashion",
-      desc: "Lot of fashion available here. New design will be update for every week, check out our new dress.",
-      cat:"women",
-      to:"productlist"
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/6261901/pexels-photo-6261901.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "Kids Fashion",
-      desc: "We have many fashion and color for your kids. New design will arrive soon, and see our latest t-shirt.",
-      cat:"kids",
-      to:"productlist"
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      title: "Man Fashion",
-      desc: "Cool and many fashion for you. Design by popular fashion stylish, we have new model of jacket, grab now.",
-      cat:"men",
-      to:"productlist"
-    },
-  ];
-
-  export const popularProducts = [
-    {
-      id:'1',
-      title: "Ripped Jeans Jacket",
-      desc: "Ripped Jeans Jacket, made by selected jeans. 100% cotton and comfort to use all day long, order now!.",
-      img:"https://m.media-amazon.com/images/I/51MjdWG7PKL.jpg",
-      categories: [ "men" ],
-      size: [ "XL", "XXL", "3XL" ],
-      color: ["Blue", "Red", "Black" ],
-      price: 1100000
-    },
-    {
-      id:'2',
-      title: "Panorama T-Shirt",
-      desc: "Panorama T-Shirt, cool design and color. 100% cotton and comfort to use all day long, order now!.",
-      img:"https://m.media-amazon.com/images/I/61RwO0gQ8HL._AC_UX385_.jpg",
-      categories: ["kids", "men", "women" ],
-      size: [ "L", "XL", "XXL", "3XL" ],
-      color: ["Green"],
-      price: 102000
-    },
-    {
-      id:'3',
-      title: "Red Skull Jacket",
-      desc: "Red Skull Jacket, cool design and color. 100% cotton and comfort to use all day long, order now!.",
-      img:"https://m.media-amazon.com/images/I/71qI-CZNx5L._AC_UX385_.jpg",
-      categories: ["kids", "men" ],
-      size: [ "S", "XS", "L", "XL" ],
-      color: ["Red"],
-      price: 200000
-    },
-    {
-      id:'4',
-      title: "Adidas Hat",
-      desc: "Adidas Hat, cool design and color. 100% cotton and comfort to use all day long, order now!.",
-      img:"https://stylecaster.com/wp-content/uploads/2020/05/adidas-hat-amazon.jpg",
-      categories: ["women", "men" ],
-      size: [ "XL", "XXL", "3XL" ],
-      color: ["Orange", "White" ],
-      price: 100000
-    },
-    {
-      id:'5',
-      title: "Nike Shoes",
-      desc: "Nike Shoes, cool design and color. 100% cotton and comfort to use all day long, order now!.",
-      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1622654199-nike-men-s-downshifter-9-running-shoe-1622654179.jpg?crop=1xw:1xh;center,top&resize=480:*",
-      categories: ["kids", "men" ],
-      size: [ "L", "XL", "XXL", "3XL" ],
-      color: ["Black", "White" ],
-      price: 1200000
-    },
-    {
-      id:'6',
-      title: "Smile Hat",
-      desc: "Smile Hat, cool design and color. 100% cotton and comfort to use all day long, order now!.",
-      img:"https://m.media-amazon.com/images/I/71Bv3BdlEpL._AC_UX385_.jpg",
-      categories: ["kids", "women" ],
-      size: ["S", "XS", "L", "XL", "XXL", "3XL" ],
-      color: ["Black", "White" ],
-      price: 90000
-    },
-  ]
+export const popularProducts = [
+  {
+    id: "1",
+    title: "Ripped Jeans Jacket",
+    desc: "Ripped Jeans Jacket, made by selected jeans. 100% cotton and comfort to use all day long, order now!.",
+    img: "https://m.media-amazon.com/images/I/51MjdWG7PKL.jpg",
+    categories: ["men"],
+    size: ["XL", "XXL", "3XL"],
+    color: ["Blue", "Red", "Black"],
+    price: 1100000,
+  },
+  {
+    id: "2",
+    title: "Panorama T-Shirt",
+    desc: "Panorama T-Shirt, cool design and color. 100% cotton and comfort to use all day long, order now!.",
+    img: "https://m.media-amazon.com/images/I/61RwO0gQ8HL._AC_UX385_.jpg",
+    categories: ["kids", "men", "women"],
+    size: ["L", "XL", "XXL", "3XL"],
+    color: ["Green"],
+    price: 102000,
+  },
+  {
+    id: "3",
+    title: "Red Skull Jacket",
+    desc: "Red Skull Jacket, cool design and color. 100% cotton and comfort to use all day long, order now!.",
+    img: "https://m.media-amazon.com/images/I/71qI-CZNx5L._AC_UX385_.jpg",
+    categories: ["kids", "men"],
+    size: ["S", "XS", "L", "XL"],
+    color: ["Red"],
+    price: 200000,
+  },
+  {
+    id: "4",
+    title: "Adidas Hat",
+    desc: "Adidas Hat, cool design and color. 100% cotton and comfort to use all day long, order now!.",
+    img: "https://stylecaster.com/wp-content/uploads/2020/05/adidas-hat-amazon.jpg",
+    categories: ["women", "men"],
+    size: ["XL", "XXL", "3XL"],
+    color: ["Orange", "White"],
+    price: 100000,
+  },
+  {
+    id: "5",
+    title: "Nike Shoes",
+    desc: "Nike Shoes, cool design and color. 100% cotton and comfort to use all day long, order now!.",
+    img: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1622654199-nike-men-s-downshifter-9-running-shoe-1622654179.jpg?crop=1xw:1xh;center,top&resize=480:*",
+    categories: ["kids", "men"],
+    size: ["L", "XL", "XXL", "3XL"],
+    color: ["Black", "White"],
+    price: 1200000,
+  },
+  {
+    id: "6",
+    title: "Smile Hat",
+    desc: "Smile Hat, cool design and color. 100% cotton and comfort to use all day long, order now!.",
+    img: "https://m.media-amazon.com/images/I/71Bv3BdlEpL._AC_UX385_.jpg",
+    categories: ["kids", "women"],
+    size: ["S", "XS", "L", "XL", "XXL", "3XL"],
+    color: ["Black", "White"],
+    price: 90000,
+  },
+];
